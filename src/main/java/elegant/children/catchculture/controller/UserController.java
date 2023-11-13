64845ab7e66a02bbdd67e8e51d4e0a63792467d7
smu207 <2019111988@dongguk.edu>
2023-11-13 18:29:52 +0900

    test: yml local 복구

--- src/main/java/elegant/children/catchculture/controller/UserController.java
+++ src/main/java/elegant/children/catchculture/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity<User> userTest(@AuthenticationPrincipal User user) {
         if(user == null) {
-            throw new RuntimeException("유저가 없습니다.2");
+            throw new RuntimeException("유저가 없습니다.3");
         }
         return ResponseEntity.ok(user);
     }
