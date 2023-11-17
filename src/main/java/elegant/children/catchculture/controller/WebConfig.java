f7790e3352c0f1a7e6e8ce1d047f53e9ac80eed9
smu207 <2019111988@dongguk.edu>
2023-11-17 18:35:28 +0900

    feat: 쿠키 인증요청 허용 해제

--- src/main/java/elegant/children/catchculture/controller/WebConfig.java
+++ src/main/java/elegant/children/catchculture/controller/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
