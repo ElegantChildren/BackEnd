f280852c2d6baa3ef6d943c260f62558e6dd3d6a
smu207 <2019111988@dongguk.edu>
2023-11-21 00:53:09 +0900

    Update WebConfig.java

--- src/main/java/elegant/children/catchculture/controller/WebConfig.java
+++ src/main/java/elegant/children/catchculture/controller/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*","http://localhost:5173") // 허용할 출처
+                .allowedOrigins("*") // 허용할 출처
                 .allowedMethods("*") // 허용할 HTTP method
 //                .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
