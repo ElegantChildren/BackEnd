498b1fd00329bc1142ab80cb231193fd06660bf7
smu207 <2019111988@dongguk.edu>
2023-11-20 14:42:34 +0900

    feat: CORS 메소드 모두 허용

--- src/main/java/elegant/children/catchculture/controller/WebConfig.java
+++ src/main/java/elegant/children/catchculture/controller/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*","http://localhost:5173") // 허용할 출처
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedMethods("*") // 허용할 HTTP method
 //                .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
