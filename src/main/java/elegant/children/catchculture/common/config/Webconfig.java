908d0ef0366b22b567efe20f062c8889a6f7c2f4
smu207 <2019111988@dongguk.edu>
2023-11-22 12:54:41 +0900

    test: webconfig 이전 파일 다시 테스트

---
+++ src/main/java/elegant/children/catchculture/common/config/Webconfig.java
@@ -0,0 +1,15 @@
+package elegant.children.catchculture.common.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedMethods("*") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
