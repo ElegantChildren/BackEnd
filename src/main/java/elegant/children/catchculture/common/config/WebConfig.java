113df8b37de19fb323bbc9cf04256d8174cf7154
smu207 <2019111988@dongguk.edu>
2023-11-22 09:36:11 +0900

    feat: webconfig 클래스 config패키지 내부로 이동

--- src/main/java/elegant/children/catchculture/common/config/WebConfig.java
+++ src/main/java/elegant/children/catchculture/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package elegant.children.catchculture.controller;
+package elegant.children.catchculture.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
