277af6d26d0c881c6fdacd714533ee6ef6377407
naver0504 <qortndwls1234@gmail.com>
2023-11-22 19:26:32 +0900

    예외 처리

--- src/main/java/elegant/children/catchculture/common/aop/AuthenticatedVisitAuthAspect.java
+++ src/main/java/elegant/children/catchculture/common/aop/AuthenticatedVisitAuthAspect.java
@@ -3,6 +3,7 @@ package elegant.children.catchculture.common.aop;
 import elegant.children.catchculture.common.exception.CustomException;
 import elegant.children.catchculture.common.exception.ErrorCode;
 import elegant.children.catchculture.entity.user.User;
+import elegant.children.catchculture.repository.culturalEvent.CulturalEventQueryRepository;
 import elegant.children.catchculture.repository.visiatAuth.VisitAuthRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,22 +17,27 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AuthenticatedVisitAuthAspect {
 
-
+    private final CulturalEventQueryRepository culturalEventQueryRepository;
     private final VisitAuthRepository visitAuthRepository;
 
     @Before("@annotation(elegant.children.catchculture.common.annotation.AuthenticatedVisitAuth) && args(culturalEventId, user)")
     public void isCulturalEventAuthenticated(final int culturalEventId, final User user ) {
-        visitAuthRepository.isAuthenticated(user.getId(), culturalEventId)
-                .ifPresentOrElse(
-                        isAuthenticated -> {
-                            if(!isAuthenticated)
-                                throw new CustomException(ErrorCode.NOT_AUTHENTICATED);
-                        },
-                        () -> {
-                            throw new CustomException(ErrorCode.NOT_AUTHENTICATED);
-                        }
-                );
+        if (culturalEventQueryRepository.existById(culturalEventId)) {
 
+            visitAuthRepository.isAuthenticated(user.getId(), culturalEventId)
+                    .ifPresentOrElse(
+                            isAuthenticated -> {
+                                if (!isAuthenticated)
+                                    throw new CustomException(ErrorCode.NOT_AUTHENTICATED);
+                            },
+                            () -> {
+                                throw new CustomException(ErrorCode.NOT_AUTHENTICATED);
+                            }
+                    );
+
+        } else {
+            throw new CustomException(ErrorCode.INVALID_EVENT_ID);
+        }
 
 
     }
