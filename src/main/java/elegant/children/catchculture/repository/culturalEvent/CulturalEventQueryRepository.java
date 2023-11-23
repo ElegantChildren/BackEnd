c5a597bd09d4b56da5130098ce7e56fa769aed9e
naver0504 <qortndwls1234@gmail.com>
2023-11-23 13:18:12 +0900

    문화 행사 상세 정보 조회 메소드 수정

--- src/main/java/elegant/children/catchculture/repository/culturalEvent/CulturalEventQueryRepository.java
+++ src/main/java/elegant/children/catchculture/repository/culturalEvent/CulturalEventQueryRepository.java
@@ -1,16 +1,19 @@
 package elegant.children.catchculture.repository.culturalEvent;
 
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import elegant.children.catchculture.common.constant.Classification;
 import elegant.children.catchculture.common.constant.SortType;
 import elegant.children.catchculture.dto.culturalEvent.response.CulturalEventDetailsResponseDTO;
 import elegant.children.catchculture.dto.culturalEvent.response.CulturalEventListResponseDTO;
 import elegant.children.catchculture.entity.culturalevent.Category;
+import elegant.children.catchculture.entity.culturalevent.QCulturalEvent;
 import elegant.children.catchculture.entity.interaction.LikeStar;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +26,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.querydsl.core.types.ExpressionUtils.count;
 import static elegant.children.catchculture.entity.culturalevent.QCulturalEvent.*;
 import static elegant.children.catchculture.entity.interaction.QInteraction.*;
 import static elegant.children.catchculture.entity.visitauth.QVisitAuth.*;
@@ -38,7 +42,7 @@ public class CulturalEventQueryRepository {
         return queryFactory.selectOne()
                 .from(culturalEvent)
                 .where(
-                        culturalEvent.id.eq(culturalEventId)
+                        culturalEventIdEq(culturalEventId)
                 )
                 .fetchOne() != null;
     }
@@ -48,17 +52,34 @@ public class CulturalEventQueryRepository {
                         culturalEvent.culturalEventDetail,
                         visitAuth.isAuthenticated.as("isAuthenticated"),
                         culturalEvent.likeCount,
-                        culturalEvent.viewCount)
+                        ExpressionUtils.as(
+                                JPAExpressions.select(count(interaction))
+                                        .from(interaction)
+                                        .where(
+                                                interactionLikeStarEq(LikeStar.STAR),
+                                                interaction.culturalEvent.id.eq(culturalEventId)
+                                        )
+                                , "bookmarkCount")
+                        )
                 )
                 .from(culturalEvent)
                 .leftJoin(visitAuth)
                 .on(
                         culturalEvent.id.eq(visitAuth.culturalEvent.id),
-                        visitAuth.user.id.eq(userId))
+                        visitAuth.user.id.eq(userId)
+                )
                 .where(
-                        culturalEvent.id.eq(culturalEventId)
+                        culturalEventIdEq(culturalEventId)
                 )
                 .fetchOne();
+    }
+
+    private static BooleanExpression interactionLikeStarEq(final LikeStar likeStar) {
+        return interaction.likeStar.eq(likeStar);
+    }
+
+    private BooleanExpression culturalEventIdEq(final int culturalEventId) {
+        return culturalEvent.id.eq(culturalEventId);
     }
 
     public List<CulturalEventListResponseDTO> getCulturalEventMainList(final List<Integer> culturalEventIdList) {
