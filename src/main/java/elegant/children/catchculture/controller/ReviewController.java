939ce594b950fc23ca886837cfc1feb3bcbd555e
naver0504 <qortndwls1234@gmail.com>
2023-11-27 04:43:17 +0900

    리뷰 작성 API

--- src/main/java/elegant/children/catchculture/controller/ReviewController.java
+++ src/main/java/elegant/children/catchculture/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package elegant.children.catchculture.controller;
 
 import elegant.children.catchculture.common.annotation.AuthenticatedVisitAuth;
+import elegant.children.catchculture.dto.review.request.CreateReviewRequestDTO;
 import elegant.children.catchculture.dto.review.request.DeleteReviewRequestDTO;
 import elegant.children.catchculture.dto.review.request.UpdateReviewRequestDTO;
 import elegant.children.catchculture.dto.review.response.ReviewRatingResponseDTO;
@@ -9,9 +10,11 @@ import elegant.children.catchculture.entity.user.User;
 import elegant.children.catchculture.service.review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -42,18 +45,24 @@ public class ReviewController {
         return ResponseEntity.ok(reviewService.getReviewList(culturalEventId, user, lastId));
     }
 
+    @PostMapping(value = "/{culturalEventId}/my-review", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @AuthenticatedVisitAuth
+    public void createReview(final @PathVariable int culturalEventId, final @AuthenticationPrincipal User user,
+                             final @RequestPart("file") MultipartFile multipartFile, final @RequestPart("reviewDetail") CreateReviewRequestDTO createReviewRequestDTO) {
+        reviewService.createReview(culturalEventId, user, multipartFile, createReviewRequestDTO.getDescription(), createReviewRequestDTO.getRating());
+    }
     /***
      * Event 가 있어야 되고 인증이 되있어야 함.
      */
+    @PutMapping("/{culturalEventId}/my-review")
     @AuthenticatedVisitAuth
-    @PatchMapping("/{culturalEventId}/my-review")
     public void updateReviewDescription(final @PathVariable int culturalEventId, @AuthenticationPrincipal User user,
                                         final @RequestBody UpdateReviewRequestDTO updateReviewRequestDTO) {
         reviewService.updateReviewDescription(updateReviewRequestDTO.getReviewId(), updateReviewRequestDTO.getDescription());
     }
 
-    @AuthenticatedVisitAuth
     @DeleteMapping("/{culturalEventId}/my-review")
+    @AuthenticatedVisitAuth
     public void deleteReview(final @PathVariable int culturalEventId, final @AuthenticationPrincipal User user,
                              final @RequestBody DeleteReviewRequestDTO deleteReviewRequestDTO) {
         reviewService.deleteReview(deleteReviewRequestDTO.getReviewId());
