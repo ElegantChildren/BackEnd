1f4efafa22e71bed43ede9162e9e254d200eae86
naver0504 <qortndwls1234@gmail.com>
2023-11-27 04:44:17 +0900

    Transaction 분리한 Service 작성

---
+++ src/main/java/elegant/children/catchculture/service/review/ReviewTransactionService.java
@@ -0,0 +1,54 @@
+package elegant.children.catchculture.service.review;
+
+import elegant.children.catchculture.common.exception.CustomException;
+import elegant.children.catchculture.common.exception.ErrorCode;
+import elegant.children.catchculture.entity.pointhistory.PointChange;
+import elegant.children.catchculture.entity.review.Review;
+import elegant.children.catchculture.entity.user.User;
+import elegant.children.catchculture.event.review.CreateReviewEvent;
+import elegant.children.catchculture.repository.review.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReviewTransactionService {
+
+    private final ReviewRepository reviewRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Transactional
+    public void updateReviewDescription(final int reviewId, final String description) {
+        reviewRepository.findById(reviewId)
+                .ifPresentOrElse(review -> {
+                    reviewRepository.updateReviewDescription(reviewId, description);
+                }, () -> {
+                    throw new CustomException(ErrorCode.INVALID_REVIEW_ID);
+                });
+    }
+
+    @Transactional
+    public void deleteReview(final int reviewId) {
+        reviewRepository.findById(reviewId)
+                .ifPresentOrElse(review -> {
+                    reviewRepository.deleteReviewById(reviewId);
+                }, () -> {
+                    throw new CustomException(ErrorCode.INVALID_REVIEW_ID);
+                });
+    }
+
+    @Transactional
+    public void createReview(final Review review, final User user) {
+
+        reviewRepository.save(review);
+        final PointChange pointChange = PointChange.REVIEW;
+        user.updatePoint(pointChange.getPoint());
+//        userRepository.updateUserPoint(user.getId(), user.getPoint() + 50);
+        applicationEventPublisher.publishEvent(new CreateReviewEvent(pointChange, user));
+
+    }
+}
