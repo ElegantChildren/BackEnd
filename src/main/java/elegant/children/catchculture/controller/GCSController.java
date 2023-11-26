ae7ff3e7a6c0f9a76d90a758f8a397bd38ef146e
naver0504 <qortndwls1234@gmail.com>
2023-11-27 05:05:17 +0900

    GCSService 수정

--- src/main/java/elegant/children/catchculture/controller/GCSController.java
+++ src/main/java/elegant/children/catchculture/controller/GCSController.java
@@ -21,18 +21,18 @@ import java.util.UUID;
 public class GCSController {
     private final GCSService gcsService;
 
-    @PostMapping(value = "gcs/uploadImage",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadImageToStorage(
-//                                                        @RequestParam("bucketName") String bucketName,
-//                                                       @RequestParam("objectName") String objectName,
-//                                                        @RequestParam("type") String type,
-                                                       @RequestParam("file") MultipartFile file) throws IOException {
-        String fileName = UUID.randomUUID().toString();
-        String bucketName = "elegant-bucket";
-        String contentType = file.getContentType();
-        GCSImageDTO dto = new GCSImageDTO(bucketName, fileName, file, contentType);
-//        GCSImageDTO dto = new GCSImageDTO(bucketName, fileName, file, type, contentType);
-        Blob uploadedFile = gcsService.uploadImageToGCS2(dto);
-        return ResponseEntity.ok("https://storage.googleapis.com/elegant-bucket/" + uploadedFile.getName());
-    }
+//    @PostMapping(value = "gcs/uploadImage",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    public ResponseEntity<String> uploadImageToStorage(
+////                                                        @RequestParam("bucketName") String bucketName,
+////                                                       @RequestParam("objectName") String objectName,
+////                                                        @RequestParam("type") String type,
+//                                                       @RequestParam("file") MultipartFile file) throws IOException {
+//        String fileName = UUID.randomUUID().toString();
+//        String bucketName = "elegant-bucket";
+//        String contentType = file.getContentType();
+//        GCSImageDTO dto = new GCSImageDTO(bucketName, fileName, file, contentType);
+////        GCSImageDTO dto = new GCSImageDTO(bucketName, fileName, file, type, contentType);
+//        Blob uploadedFile = gcsService.uploadImageToGCS(dto);
+//        return ResponseEntity.ok(uploadedFile.toString());
+//    }
 }
