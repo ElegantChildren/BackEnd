0f171037c0910ee77771292559b5571df4e88e24
smu207 <2019111988@dongguk.edu>
2023-11-28 06:10:28 +0900

    문화행사 제보 추가(미완성)

--- src/main/java/elegant/children/catchculture/dto/culturalEvent/response/CulturalEventReportDTO.java
+++ src/main/java/elegant/children/catchculture/dto/culturalEvent/response/CulturalEventReportDTO.java
@@ -1,20 +1,32 @@
 package elegant.children.catchculture.dto.culturalEvent.response;
 
-import org.springframework.web.multipart.MultipartFile;
-
-//import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import elegant.children.catchculture.entity.culturalevent.Category;
+import lombok.*;
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class CulturalEventReportDTO {
+@Getter
+@ToString
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class CulturalEventReportDTO implements Serializable {
+    private Category category;
+    private String reservationLink;
     private String eventName;
     private String eventLocation;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
     private boolean isFree;
     private String description;
     private String snsAddress;
     private String phoneNumber;
-    private MultipartFile eventPhoto;
-    private String directions;
+//    private List<MultipartFile> fileList;
+//    private List<String> storedFileUrl;
+    private String wayToCome;
 
 }
--- src/main/java/elegant/children/catchculture/service/culturalEvent/CulturalEventReportService.java
+++ src/main/java/elegant/children/catchculture/service/culturalEvent/CulturalEventReportService.java
@@ -1,8 +1,49 @@
 package elegant.children.catchculture.service.culturalEvent;
 
+import elegant.children.catchculture.dto.culturalEvent.response.CulturalEventReportDTO;
+import elegant.children.catchculture.entity.culturalevent.CulturalEventDetail;
+import elegant.children.catchculture.entity.eventreport.EventReport;
+import elegant.children.catchculture.entity.user.User;
+import elegant.children.catchculture.repository.culturalEvent.CulturalEventReportRepository;
+import elegant.children.catchculture.service.GCS.GCSService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CulturalEventReportService {
+
+
+    private final CulturalEventReportRepository culturalEventReportRepository;
+    private final GCSService gcsService;
+
+
+    public String createEventReport(CulturalEventReportDTO culturalEventReportDTO, List<MultipartFile> fileList, User user) throws IOException {
+        CulturalEventDetail culturalEventDetail = CulturalEventDetail.builder()
+                .category(culturalEventReportDTO.getCategory())
+                .reservationLink(culturalEventReportDTO.getReservationLink())
+                .title(culturalEventReportDTO.getEventName())
+                .place(culturalEventReportDTO.getEventLocation())
+                .startDate(culturalEventReportDTO.getStartDate().atStartOfDay())
+                .endDate(culturalEventReportDTO.getEndDate().atStartOfDay())
+                .isFree(culturalEventReportDTO.isFree())
+                .description(culturalEventReportDTO.getDescription())
+                .sns(culturalEventReportDTO.getSnsAddress())
+                .telephone(culturalEventReportDTO.getPhoneNumber())
+                .wayToCome(culturalEventReportDTO.getWayToCome())
+                .storedFileUrl(gcsService.uploadImages(fileList))
+                .build();
+        EventReport eventReport = EventReport.builder()
+                .culturalEventDetail(culturalEventDetail)
+                .user(user)
+                .build();
+        culturalEventReportRepository.save(eventReport);
+
+        return eventReport.getCulturalEventDetail().toString();
+    }
 
 }
