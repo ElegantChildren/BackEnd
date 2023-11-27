103049a552b97a6e38cf77fcfcd705d1b932ff25
naver0504 <qortndwls1234@gmail.com>
2023-11-28 03:42:12 +0900

    Event handle 메소드 작성

--- src/main/java/elegant/children/catchculture/service/user/UserService.java
+++ src/main/java/elegant/children/catchculture/service/user/UserService.java
@@ -1,14 +1,16 @@
 package elegant.children.catchculture.service.user;
 
 import elegant.children.catchculture.common.constant.Classification;
-import elegant.children.catchculture.common.constant.SortType;
 import elegant.children.catchculture.dto.culturalEvent.response.CulturalEventListResponseDTO;
 import elegant.children.catchculture.entity.culturalevent.Category;
 import elegant.children.catchculture.entity.user.User;
+import elegant.children.catchculture.event.AuthenticateVisitAuthEvent;
+import elegant.children.catchculture.event.CreateCulturalEvent;
 import elegant.children.catchculture.repository.culturalEvent.CulturalEventQueryRepository;
 import elegant.children.catchculture.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,16 +29,36 @@ public class UserService {
     private final CulturalEventQueryRepository culturalEventQueryRepository;
 
     @Transactional
+    @EventListener
+    public void handleCreateCulturalEvent(final CreateCulturalEvent createCulturalEvent) {
+        log.info("handleCreateCulturalEvent");
+        updateUserPoint(createCulturalEvent.getUser(), createCulturalEvent.getPointChange().getPoint());
+    }
+
+    @Transactional
+    @EventListener
+    public void handleCreateCulturalEvent(final AuthenticateVisitAuthEvent authenticateVisitAuthEvent) {
+        log.info("handleCreateCulturalEvent");
+        updateUserPoint(authenticateVisitAuthEvent.getUser(), authenticateVisitAuthEvent.getPointChange().getPoint());
+    }
+
+
+
+    @Transactional
     public void updateUserNickname(final User user) {
         userRepository.updateNickname(user.getNickname(), user.getId());
     }
 
     public Page<CulturalEventListResponseDTO> getCulturalEventListWithUser(final User user,
                                                                            final int offset,
-                                                                           final List<Category> categoryList,
-                                                                           final Classification classification) {
+                                                                           final List<Category> categoryList,final Classification classification) {
         return culturalEventQueryRepository.getCulturalEventResponseDTOWithUser( categoryList, user.getId(),
                                                                                 createPageRequest(offset), classification);
+    }
+
+
+    private void updateUserPoint(final User user, final int point) {
+        userRepository.updateUserPoint(point, user.getId());
     }
 
 
