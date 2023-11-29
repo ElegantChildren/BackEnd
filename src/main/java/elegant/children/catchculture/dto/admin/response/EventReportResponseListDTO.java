c8f669a4ed8af3670f71dfc429de7fcacd5a299d
smu207 <62317130+smu207@users.noreply.github.com>
2023-11-29 17:30:18 +0900

    test: evetn report response dto nickname int -> string으로 변경

--- src/main/java/elegant/children/catchculture/dto/admin/response/EventReportResponseListDTO.java
+++ src/main/java/elegant/children/catchculture/dto/admin/response/EventReportResponseListDTO.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class EventReportResponseListDTO {
 
     private int id;
-    private int nickname;
+    private String nickname;
     private String title;
     private LocalDateTime createdAt;
 
