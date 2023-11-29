d531cf9df7705b6153340b6b725c0bd812740d78
smu207 <62317130+smu207@users.noreply.github.com>
2023-11-29 17:19:22 +0900

    test: admin eventlist 파라미터 수정

--- src/main/java/elegant/children/catchculture/controller/AdminController.java
+++ src/main/java/elegant/children/catchculture/controller/AdminController.java
@@ -42,7 +42,7 @@ public class AdminController {
 
     @AdminUser
     @GetMapping("/event-report/list")
-    public ResponseEntity<Slice<EventReportResponseListDTO>> getEventReportList(final int lastId) {
+    public ResponseEntity<Slice<EventReportResponseListDTO>> getEventReportList(final @RequestParam int lastId) {
         return ResponseEntity.ok(eventReportService.getEventReportList(lastId));
     }
 
