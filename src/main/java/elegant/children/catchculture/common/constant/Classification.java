64a471741df3004ba44e623f7af71d7fec2359e2
naver0504 <qortndwls1234@gmail.com>
2023-11-30 12:54:34 +0900

    불필요한 ; 삭제

--- src/main/java/elegant/children/catchculture/common/constant/Classification.java
+++ src/main/java/elegant/children/catchculture/common/constant/Classification.java
@@ -3,5 +3,5 @@ package elegant.children.catchculture.common.constant;
 public enum Classification {
     VISIT_AUTH,
     LIKE,
-    STAR;
+    STAR
 }
--- src/main/java/elegant/children/catchculture/entity/user/Role.java
+++ src/main/java/elegant/children/catchculture/entity/user/Role.java
@@ -1,12 +1,8 @@
 package elegant.children.catchculture.entity.user;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public enum Role {
 
     USER,
-    ADMIN;
+    ADMIN
 
 }
