13656cb0142ab6daabcff01df8535405964ac2d1
smu207 <2019111988@dongguk.edu>
2023-11-30 14:33:27 +0900

    feat: 제보 category추가

--- src/main/java/elegant/children/catchculture/dto/culturalEvent/response/CulturalEventReportDTO.java
+++ src/main/java/elegant/children/catchculture/dto/culturalEvent/response/CulturalEventReportDTO.java
@@ -1,6 +1,7 @@
 package elegant.children.catchculture.dto.culturalEvent.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import elegant.children.catchculture.entity.culturalevent.Category;
 import lombok.*;
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ public class CulturalEventReportDTO implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private Boolean isFree;
+    private Category category;
     @NonNull
     private String description;
     private String snsAddress;
