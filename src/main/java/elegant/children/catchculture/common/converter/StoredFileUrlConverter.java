6a5f3a58a931f780cc3d68a91012a2145e1ee33f
smu207 <2019111988@dongguk.edu>
2023-12-02 07:16:40 +0900

    test: UrlConverter null처리 테스트

--- src/main/java/elegant/children/catchculture/common/converter/StoredFileUrlConverter.java
+++ src/main/java/elegant/children/catchculture/common/converter/StoredFileUrlConverter.java
@@ -11,11 +11,23 @@ public class StoredFileUrlConverter implements AttributeConverter<List<String>, 
     private final String Delimiter = ", ";
     @Override
     public String convertToDatabaseColumn(final List<String> attribute) {
+
+        if (attribute == null) {
+            return null;
+        }
+
         return attribute.stream().collect(Collectors.joining(Delimiter));
+
     }
 
     @Override
     public List<String> convertToEntityAttribute(final String dbData) {
+
+        if (dbData == null) {
+            return null;
+        }
+
         return Arrays.asList(dbData.split(Delimiter));
+
     }
 }
