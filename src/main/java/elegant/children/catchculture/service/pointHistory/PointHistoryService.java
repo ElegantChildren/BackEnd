0a55e8acecc234dd1fcaee3d206a0bf07f7173fc
smu207 <2019111988@dongguk.edu>
2023-12-03 04:19:55 +0900

    feat: 상품 구매 api 작성

--- src/main/java/elegant/children/catchculture/service/pointHistory/PointHistoryService.java
+++ src/main/java/elegant/children/catchculture/service/pointHistory/PointHistoryService.java
@@ -9,8 +9,10 @@ import elegant.children.catchculture.event.CreateCulturalEvent;
 import elegant.children.catchculture.repository.pointHistory.PointHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.data.domain.Page;
@@ -21,8 +23,9 @@ import org.springframework.data.domain.Pageable;
 @RequiredArgsConstructor
 @Slf4j
 public class PointHistoryService {
-    
+
     private final PointHistoryRepository pointHistoryRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleCreateReviewEvent(final CreateCulturalEvent createCulturalEvent) {
@@ -53,4 +56,17 @@ public class PointHistoryService {
     public Page<PointHistoryResponseDTO> getPointHistoryForUser(final User user, final Pageable pageable) {
         return pointHistoryRepository.findHistories(user.getId(), pageable);
     }
+
+    @Transactional
+    public String purchaseReward(final User user, final PointChange pointChange) {
+        if(user.getPoint() + pointChange.getPoint()<0){
+            return "포인트가 부족합니다.";
+        }
+        if(pointChange!= PointChange.COFFEE && pointChange!= PointChange.IMO){
+            return "잘못된 값을 입력하셨습니다.";
+        }
+        applicationEventPublisher.publishEvent(new CreatePointHistoryEvent(pointChange, user));
+        return pointChange.getDescription() + "성공";
+    }
+
 }
