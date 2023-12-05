3b89f56110000f03bdcee1a5c4568a5651f63ba7
naver0504 <qortndwls1234@gmail.com>
2023-12-05 14:35:46 +0900

    deleteFileEvent 스케쥴링 작성

--- src/main/java/elegant/children/catchculture/CatchCultureApplication.java
+++ src/main/java/elegant/children/catchculture/CatchCultureApplication.java
@@ -2,8 +2,10 @@ package elegant.children.catchculture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케줄러 사용
 public class CatchCultureApplication {
 
 	public static void main(String[] args) {
--- src/main/java/elegant/children/catchculture/service/fileEvent/FileEventService.java
+++ src/main/java/elegant/children/catchculture/service/fileEvent/FileEventService.java
@@ -2,24 +2,37 @@ package elegant.children.catchculture.service.fileEvent;
 
 import elegant.children.catchculture.entity.fileEvent.FileEvent;
 import elegant.children.catchculture.repository.fileEvent.FileEventRepository;
+import elegant.children.catchculture.service.GCS.GCSService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FileEventService {
 
     private final FileEventRepository fileEventRepository;
+    private final GCSService gcsService;
 
 
     @Transactional
     @EventListener
     public void handleFileEvent(final List<FileEvent> fileEventList) {
         fileEventRepository.saveAll(fileEventList);
+    }
+
+    @Transactional
+    @Scheduled(fixedRate = 1000 * 60 * 60) //1시간마다
+    public void deleteFileEvent() throws IOException {
+
+        gcsService.deleteFileFromGCSByUrlList(fileEventRepository.findAll().stream().map(FileEvent::getFileName).collect(Collectors.toList()));
+        fileEventRepository.deleteAll();
     }
 
 
