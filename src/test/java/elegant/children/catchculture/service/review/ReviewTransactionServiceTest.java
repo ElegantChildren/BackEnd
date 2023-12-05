cf9efeff02c7f95cb5c39acd083873ab6a6244b2
naver0504 <qortndwls1234@gmail.com>
2023-12-05 17:54:40 +0900

    사용하지 않는 Test 삭제

--- src/test/java/elegant/children/catchculture/service/review/ReviewTransactionServiceTest.java
+++ src/test/java/elegant/children/catchculture/service/review/ReviewTransactionServiceTest.java
@@ -13,10 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.test.context.event.ApplicationEvents;
-import org.springframework.test.context.event.RecordApplicationEvents;
 
 import java.util.Optional;
 
