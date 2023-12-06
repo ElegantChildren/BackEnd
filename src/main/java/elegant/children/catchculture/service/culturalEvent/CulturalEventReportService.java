34335688cbb2e5b460aa8089ac7483c02ce3a34d
smu207 <62317130+smu207@users.noreply.github.com>
2023-12-07 00:21:50 +0900

    fix: 위도경도 반대로 들어가게 수정(event report)

--- src/main/java/elegant/children/catchculture/service/culturalEvent/CulturalEventReportService.java
+++ src/main/java/elegant/children/catchculture/service/culturalEvent/CulturalEventReportService.java
@@ -33,8 +33,8 @@ public class CulturalEventReportService {
 
     @Transactional
     public String createEventReport(CulturalEventReportDTO culturalEventReportDTO, List<MultipartFile> fileList, User user) throws IOException {
-        Double latitude = Arrays.stream(geoCodeService.geocoder(culturalEventReportDTO.getEventLocation()).getResults()).map(result -> result.getGeometry().getLocation().getLat()).findFirst().orElseThrow();
-        Double longitude = Arrays.stream(geoCodeService.geocoder(culturalEventReportDTO.getEventLocation()).getResults()).map(result -> result.getGeometry().getLocation().getLng()).findFirst().orElseThrow();
+        Double latitude = Arrays.stream(geoCodeService.geocoder(culturalEventReportDTO.getEventLocation()).getResults()).map(result -> result.getGeometry().getLocation().getLng()).findFirst().orElseThrow();
+        Double longitude = Arrays.stream(geoCodeService.geocoder(culturalEventReportDTO.getEventLocation()).getResults()).map(result -> result.getGeometry().getLocation().getLat()).findFirst().orElseThrow();
         List<String> storedFileUrl = new ArrayList<>();
         if (fileList != null && !fileList.isEmpty()) {
             storedFileUrl = gcsService.uploadImages(fileList);
