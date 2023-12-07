cd8505cf90d8941fb8f1c90c96e1cda30ba09ad8
naver0504 <qortndwls1234@gmail.com>
2023-12-07 14:06:00 +0900

    쿼리 에러 수정

--- src/main/java/elegant/children/catchculture/repository/visiatAuth/VisitAuthRepository.java
+++ src/main/java/elegant/children/catchculture/repository/visiatAuth/VisitAuthRepository.java
@@ -4,6 +4,7 @@ import elegant.children.catchculture.entity.visitauth.VisitAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,15 +16,16 @@ public interface VisitAuthRepository extends JpaRepository<VisitAuth, Integer> {
     @Query("select va.isAuthenticated from VisitAuth as va where va.user.id = :userId and va.culturalEvent.id = :culturalEventId")
     Optional<Boolean> isAuthenticated(final int userId, final int culturalEventId);
 
-    @Query("select va from VisitAuth as va join fetch User as u on va.user.id = u.id join fetch CulturalEvent as ce on va.culturalEvent.id = ce.id " +
+    @Query("select va from VisitAuth va " +
+            "join fetch va.user u " +
             "where va.id = :id and u.id = :userId and va.culturalEvent.id = :culturalEventId")
-    Optional<VisitAuth> findByIdWithUserAAndCulturalEvent(final int id, final int userId, final int culturalEventId);
+    Optional<VisitAuth> findByIdWithUserAndCulturalEvent(final @Param("id") int id, final @Param("userId") int userId, final @Param("culturalEventId") int culturalEventId);
 
     @Query("select va from VisitAuth as va " +
             "join fetch va.user as u " +
             "join fetch va.culturalEvent as ce " +
             "where u.id = :userId and ce.id = :culturalEventId")
-    Optional<VisitAuth> findByUserAndCulturalEvent(final int userId, final int culturalEventId);
+    Optional<VisitAuth> findByUserAndCulturalEvent(final @Param("userId") int userId, final @Param("culturalEventId") int culturalEventId);
 
     @Modifying(clearAutomatically = true)
     @Query("delete from VisitAuth va where va.user.id = :userId")
